39258a0afd900076408d8f465e3245437359c77e
Jossemand <s224564@student.dtu.dk>
2022-10-28 17:42:58 +0200

    Constructor and variables created for class Account and Square

--- src/Game/Square.java
+++ src/Game/Square.java
@@ -2,5 +2,13 @@ package Game;
 
 public class Square {
 
-}
+    private String name;
+    private int balanceUpdate;
+    private int BalanceUpdateText;
+
+    public Square(String name, int balanceUpdateText, int balanceUpdate) {
+        this.name = name;
+        this.balanceUpdate = balanceUpdate;
+
+    }
 
