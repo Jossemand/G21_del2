c52091dffee2be39f9ebf18b3e22b8d9ee0581b8
Jacob Baek <jacob.moselund@hotmail.com>
2022-10-28 17:48:41 +0200

    Classes+Variables Player, Dice, WinCon

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,4 +1,15 @@
 package Game;
+import java.util.Scanner;
 
-public class Player {
+    public class Player {
+        private String name;
+        Scanner input = new Scanner(System.in);
+        Dice dice  = new Dice();
+        private int position;
+        private int turnCounter = 1;
+
+        public Player(String name) {
+            this.name = name;
+            this.position =  0;
+        }
 }
--- src/Game/WinCon.java
+++ src/Game/WinCon.java
@@ -1,4 +1,11 @@
 package Game;
 
 public class WinCon {
+        private boolean winnerIsFound;
+        private String winnerName;
+
+        public WinCon() {
+            this.winnerIsFound = false;
+
+        }
 }
