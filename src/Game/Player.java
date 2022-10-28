eaf224cca5b2269b00cb12e1ebf54d1e30a12d53
Jacob Baek <jacob.moselund@hotmail.com>
2022-10-28 18:06:46 +0200

    Math.abs added in Switch loop

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -65,15 +65,15 @@ import java.util.Scanner;
                     break;
                 case  4:
                     System.out.println(player.getName() + " moves to " + square[2].getSquareName());
-                    System.out.println("You have a successful day begging in front of the palace gates. You gain " + square[2].getBalanceUpdate());
+                    System.out.println("You have a successful day begging in front of the palace gates. You gain " + Math.abs(square[2].getBalanceUpdate()));
                     break;
                 case  5:
                     System.out.println(player.getName() + " moves to " + square[3].getSquareName());
-                    System.out.println("You wonder through the cold desert. Luckily there is a merchant selling hot chocolate. You spend " + square[3].getBalanceUpdate());
+                    System.out.println("You wonder through the cold desert. Luckily there is a merchant selling hot chocolate. You spend " + Math.abs(square[3].getBalanceUpdate()));
                     break;
                 case  6:
                     System.out.println(player.getName() + " moves to " + square[4].getSquareName());
-                    System.out.println("You arrive in the safest place in the kingdom. You spend a day or two doing som errands and earn " + square[4].getBalanceUpdate());
+                    System.out.println("You arrive in the safest place in the kingdom. You spend a day or two doing som errands and earn " + Math.abs(square[4].getBalanceUpdate()));
                     break;
                 case  7:
                     System.out.println(player.getName() + " moves to " + square[5].getSquareName());
@@ -81,23 +81,23 @@ import java.util.Scanner;
                     break;
                 case  8:
                     System.out.println(player.getName() + " moves to " + square[6].getSquareName());
-                    System.out.println("You venture into the black cave in order to defeat the black bear living there. You almost die and have to pay for medical help. You lose " + square[6].getBalanceUpdate());
+                    System.out.println("You venture into the black cave in order to defeat the black bear living there. You almost die and have to pay for medical help. You lose " + Math.abs(square[6].getBalanceUpdate()));
                     break;
                 case  9:
                     System.out.println(player.getName() + " moves to " + square[7].getSquareName());
-                    System.out.println("You visit some huts in the mountain and find some dropped coins in the forests. You gain " + square[7].getBalanceUpdate());
+                    System.out.println("You visit some huts in the mountain and find some dropped coins in the forests. You gain " + Math.abs(square[7].getBalanceUpdate()));
                     break;
                 case  10:
                     System.out.println(player.getName() + " moves to " + square[8].getSquareName());
-                    System.out.println("You find youself sorrounded by werewolves that rob you. You lose " + square[8].getBalanceUpdate() + "But get an extra turn");
+                    System.out.println("You find youself sorrounded by werewolves that rob you. You lose " + Math.abs(square[8].getBalanceUpdate()) + "But get an extra turn");
                     break;
                 case  11:
                     System.out.println(player.getName() + " moves to " + square[9].getSquareName());
-                    System.out.println("You visit the local fighting pits and gamble all day. You lose " + square[9].getBalanceUpdate());
+                    System.out.println("You visit the local fighting pits and gamble all day. You lose " + Math.abs(square[9].getBalanceUpdate()));
                     break;
                 case  12:
                     System.out.println(player.getName() + " moves to " + square[10].getSquareName());
-                    System.out.println("You find gold in the mountains and sell it all! You earn " + square[10].getBalanceUpdate());
+                    System.out.println("You find gold in the mountains and sell it all! You earn " + Math.abs(square[10].getBalanceUpdate()));
                     break;
             }
         }
